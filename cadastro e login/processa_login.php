9c26690fc94a06dea6cc723c3c6b0ad3ea33e116
marcelosalvador <86106968+marcelosalvador@users.noreply.github.com>
2023-11-21 21:16:54 -0300

    Atualização do Banco de dados e cadastro

--- cadastro e login/processa_login.php
+++ cadastro e login/processa_login.php
@@ -6,13 +6,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha = $_POST["senha"];
 
     // Conecte-se ao banco de dados (substitua pelos dados de conexão reais)
-    $conexao = new mysqli("localhost", "seu_usuario", "sua_senha", "registros");
+    $conexao = new mysqli("localhost", "seu_usuario", "sua_senha", "Usuarios");
 
     if ($conexao->connect_error) {
         die("Conexão falhou: " . $conexao->connect_error);
     }
 
-    $sql = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
+    $sql = "SELECT * FROM Usuarios WHERE email = ? AND senha = ?";
     $stmt = $conexao->prepare($sql);
     $stmt->bind_param("ss", $email, $senha);
     $stmt->execute();
--- cadastro e login/registros.sql
+++ cadastro e login/registros.sql
@@ -1,7 +1,25 @@
-CREATE DATABASE registros;
-USE registros;
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    senha VARCHAR(255) NOT NULL
-);
+CREATE DATABASE Registros;
+USE Registros;
+
+CREATE TABLE Usuarios 
+( 
+ ID INT PRIMARY KEY AUTO_INCREMENT,
+ nome VARCHAR(255) NOT NULL,  
+ e-mail VARCHAR(255) NOT NULL,  
+ senha VARCHAR(255) NOT NULL,  
+); 
+
+CREATE TABLE Transtornos 
+( 
+ID_transtorno INT PRIMARY KEY AUTO_INCREMENT,
+nome_transtorno VARCHAR(255) NOT NULL,  
+); 
+
+CREATE TABLE Sofre 
+( 
+ ID INT PRIMARY KEY,
+ ID_transtorno INT PRIMARY KEY, 
+); 
+
+ALTER TABLE Sofre ADD FOREIGN KEY(ID) REFERENCES Usuarios (ID)
+ALTER TABLE Sofre ADD FOREIGN KEY(ID_transtorno) REFERENCES Transtornos (ID_transtorno)
